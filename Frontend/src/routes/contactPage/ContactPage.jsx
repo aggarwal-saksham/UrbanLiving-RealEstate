0e35abe5a4aeae743b6c4f6c25e62707798c2926
Saksham Aggarwal <saksham080305@gmail.com>
2026-03-29 16:35:39 +0530

    Fix contact page JSX markup

--- Frontend/src/routes/contactPage/ContactPage.jsx
+++ Frontend/src/routes/contactPage/ContactPage.jsx
@@ -3,64 +3,65 @@ import "./contactPage.css";
 
 const ContactPage = () => {
   return (
-    <div class="new_home_web">
-      <div class="responsive-container-block big-container">
+    <div className="new_home_web">
+      <div className="responsive-container-block big-container">
         <img
-          class="imgBG"
+          className="imgBG"
           src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw65.png"
+          alt="Decorative background"
         />
-        <div class="responsive-container-block textContainer">
-          <div class="topHead">
-            <p class="text-blk heading">
+        <div className="responsive-container-block textContainer">
+          <div className="topHead">
+            <p className="text-blk heading">
               Get in
-              <span class="orangeText">touch</span>
+              <span className="orangeText">touch</span>
             </p>
-            <div class="orangeLine" id="w-c-s-bgc_p-2-dm-id"></div>
+            <div className="orangeLine" id="w-c-s-bgc_p-2-dm-id"></div>
           </div>
-          <p class="text-blk subHeading">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna al iqua.
+          <p className="text-blk subHeading">
+            Reach out for property inquiries, listing support, or help finding
+            the right place in your preferred neighborhood.
           </p>
         </div>
-        <div class="responsive-container-block container">
+        <div className="responsive-container-block container">
           <div
-            class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-7 wk-ipadp-10 line"
+            className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-7 wk-ipadp-10 line"
             id="i69b"
           >
-            <form class="form-box">
-              <div class="container-block form-wrapper">
-                <div class="responsive-container-block">
-                  <div class="left4">
+            <form className="form-box">
+              <div className="container-block form-wrapper">
+                <div className="responsive-container-block">
+                  <div className="left4">
                     <div
-                      class="responsive-cell-block wk-ipadp-6 wk-tab-12 wk-mobile-12 wk-desk-6"
+                      className="responsive-cell-block wk-ipadp-6 wk-tab-12 wk-mobile-12 wk-desk-6"
                       id="i10mt-2"
                     >
                       <input
-                        class="input"
+                        className="input"
                         id="ijowk-2"
                         name="FirstName"
                         placeholder="First Name"
                       />
                     </div>
-                    <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
+                    <div className="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
                       <input
-                        class="input"
+                        className="input"
                         id="indfi-2"
                         name="Last Name"
                         placeholder="Last Name"
                       />
                     </div>
-                    <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
+                    <div className="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12">
                       <input
-                        class="input"
+                        className="input"
                         id="ipmgh-2"
                         name="Email"
                         placeholder="Email Address"
                       />
                     </div>
-                    <div class="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12 lastPhone">
+                    <div className="responsive-cell-block wk-desk-6 wk-ipadp-6 wk-tab-12 wk-mobile-12 lastPhone">
                       <input
-                        class="input"
+                        className="input"
                         id="imgis-2"
                         name="PhoneNumber"
                         placeholder="Phone Number"
@@ -68,19 +69,19 @@ const ContactPage = () => {
                     </div>
                   </div>
                   <div
-                    class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12"
+                    className="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12"
                     id="i634i-2"
                   >
                     <textarea
-                      class="textinput"
+                      className="textinput"
                       id="i5vyy-2"
                       placeholder="Message"
                     ></textarea>
                   </div>
                 </div>
-                <a class="send" href="#" id="w-c-s-bgc_p-1-dm-id">
+                <button className="send" type="button" id="w-c-s-bgc_p-1-dm-id">
                   Send
-                </a>
+                </button>
               </div>
             </form>
           </div>
